eb0af893d509a06e78f9d530245c46eeae03664c
Erik Reynolds <erik.j.reynolds@gmail.com>
2013-01-20 01:43:25 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitecheckr::Application.config.secret_token = 'caa161edbfc54c0aad65295862f01d428add918538f709dc19b078cd447517347af8f4700db598b44f30503962899691cdcb9082ec13b942b6f019fac2b87568'
