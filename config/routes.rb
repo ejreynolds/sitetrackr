ddcd525b9c10e64650f880e4ffce26ad089a55f9
Erik Reynolds <erik.j.reynolds@gmail.com>
2013-01-20 22:10:50 -0500

    Ran migration / added schema / removed index.html.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Sitecheckr::Application.routes.draw do
 
   resources :sites
 
-
+  root :to => 'sites#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
